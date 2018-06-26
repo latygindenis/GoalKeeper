5d49811a4baed0776c7c3d6984e271204f957b39
denis <denisss2221@gmail.com>
2018-06-26 21:10:35 +0700

    -Небольшой разбор уведомлений

--- app/src/main/java/goalkeeper/android/bignerdranch/com/goalkeeper/presentation/goalslist/recycler/GoalsAdapter.java
+++ app/src/main/java/goalkeeper/android/bignerdranch/com/goalkeeper/presentation/goalslist/recycler/GoalsAdapter.java
@@ -1,7 +1,14 @@
 package goalkeeper.android.bignerdranch.com.goalkeeper.presentation.goalslist.recycler;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +20,7 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 
 import java.util.Date;
 import java.util.ArrayList;
+import java.util.TimerTask;
 
 import goalkeeper.android.bignerdranch.com.goalkeeper.R;
 import goalkeeper.android.bignerdranch.com.goalkeeper.data.Goal;
@@ -26,7 +34,9 @@ import goalkeeper.android.bignerdranch.com.goalkeeper.presentation.goaldetail.Go
 public class GoalsAdapter extends RecyclerView.Adapter<GoalsHolder>{
     private ArrayList<Goal> goals;
     private Context context;
-
+    private AlarmManager am;
+    Intent resultIntent;
+    PendingIntent resultPendingIntent;
     public GoalsAdapter (ArrayList<Goal> goals, Context cotext){
         this.goals = goals;
         this.context = cotext;
@@ -37,6 +47,7 @@ public class GoalsAdapter extends RecyclerView.Adapter<GoalsHolder>{
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater
                 .inflate(R.layout.list_item_goal, parent, false);
+        am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         return new GoalsHolder(view);
     }
 
@@ -76,7 +87,36 @@ public class GoalsAdapter extends RecyclerView.Adapter<GoalsHolder>{
                     holder.button.setBackgroundColor(red);
                     holder.progressBar.setProgress(goal.getStatistics().getCurrent_streak());
                     notifyDataSetChanged();
+                    am.cancel(resultPendingIntent);
                 }
+
+
+                NotificationChannel notificationChannel = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    notificationChannel = new NotificationChannel("newid", "s", NotificationManager.IMPORTANCE_DEFAULT);
+                }
+                resultIntent = GoalDetailActivity.newIntent(context, goal.getUuid());
+                resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                NotificationCompat.Builder builder =
+                            new NotificationCompat.Builder(context, "newid")
+                                    .setSmallIcon(R.mipmap.ic_launcher)
+                                    .setContentTitle("Пора!")
+                                    .setContentText("Время выполнить цель: "+ goal.getTitle_goal())
+                                      .setContentIntent(resultPendingIntent)
+                        .setAutoCancel(true);
+
+                    Notification notification = builder.build();
+
+                    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+                notificationManager.notify(1, notification);
+
+                am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 3000, 5000, resultPendingIntent);
+
+
 
             }
         });
